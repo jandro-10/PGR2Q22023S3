9087b069854fdede6da042f9f116e6e6a78b59b2
jandro-10 <117784827+jandro-10@users.noreply.github.com>
2023-05-08 19:38:15 -0600

    Create Persona.cs

---
+++ Herencia/Herencia/Persona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    internal class Persona
+    {
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public DateTime fechaNacimiento { get; set; }
+
+        public override string ToString()
+        {
+            return $"hola mi nombre es {nombre};
+        }
+    }
+}
